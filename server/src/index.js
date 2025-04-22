85aca3d564bea3b1ce4e41d9526fa06d1a2674e2
Shreyansh <shreyanshloop07@gmail.com>
2025-04-23 00:38:23 +0530

    feat(server): add auth controllers, routes, and isAuthenticated middleware (#4)

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import authRoutes from './routes/auth.routes.js'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 const app = express()
+app.use(cookieParser());
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World, this is the LeetLabs server!')
+    res.send('Hello World, this is the LeetLabs server!🔥')
 })
+
+app.use("/api/v1/auth", authRoutes)
 
 app.listen(process.env.PORT|| 3000, () => {
     console.log(`Server running on port ${process.env.PORT}`)   
---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,148 @@
+import jwt from "jsonwebtoken";
+import { db } from "../utils/db.js";
+import cookieParser from "cookie-parser";
+export const isAuthenticated = async (req, res, next) => {
+  try {
+    const accessToken = req.cookies.accessToken;
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!accessToken) {
+      if (!refreshToken) {
+        return res.status(401).json({
+          status: false,
+          message: "Unauthorized access",
+        });
+      } else {
+        const refreshDecoded = jwt.verify(
+          refreshToken,
+          process.env.REFRESHTOKEN_SECRET
+        );
+        console.log(refreshDecoded);
+
+        const user = await db.user.findUnique({
+          where: { id: refreshDecoded.id },
+          select: {
+            id: true,
+            image: true,
+            name: true,
+            email: true,
+            role: true,
+          },
+        });
+
+        if (!user) {
+          return res.status(401).json({
+            status: false,
+            message: "Unauthorized access",
+          });
+        }
+
+        const newAccessToken = jwt.sign(
+            { id: user.id },
+            process.env.ACCESSTOKEN_SECRET,
+            { expiresIn: process.env.ACCESSTOKEN_EXPIRY }
+          );
+          const newRefreshToken = jwt.sign(
+            { id: user.id },
+            process.env.REFRESHTOKEN_SECRET,
+            {
+              expiresIn: process.env.REFRESHTOKEN_EXPIRY,
+            }
+          );
+    
+          user.refreshToken = newRefreshToken;
+          await db.user.update({
+            where: { id: user.id },
+            data: { refreshToken: newRefreshToken },
+          });
+    
+          const accessCookieOptions = {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: 1 * 24 * 60 * 60 * 1000,
+          };
+    
+          const refreshCookieOptions = {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+          };
+    
+          // res.cookie("jwtToken", jwtToken, cookieOptions);
+          res.cookie("accessToken", newAccessToken, accessCookieOptions);
+          res.cookie("refreshToken", newRefreshToken, refreshCookieOptions);
+    
+          req.user = refreshDecoded;
+          next();
+      }
+    } else {
+      const accessDecoded = jwt.verify(
+        accessToken,
+        process.env.ACCESSTOKEN_SECRET
+      );
+
+      const user = await db.user.findUnique({
+        where: { id: accessDecoded.id },
+        select: {
+          id: true,
+          image: true,
+          name: true,
+          email: true,
+          role: true,
+        },
+      });
+      
+      if (!user) {
+        return res.status(401).json({
+          status: false,
+          message: "Unauthorized access",
+        });
+      }
+
+      const newAccessToken = jwt.sign(
+        { id: user.id },
+        process.env.ACCESSTOKEN_SECRET,
+        { expiresIn: process.env.ACCESSTOKEN_EXPIRY }
+      );
+      const newRefreshToken = jwt.sign(
+        { id: user.id },
+        process.env.REFRESHTOKEN_SECRET,
+        {
+          expiresIn: process.env.REFRESHTOKEN_EXPIRY,
+        }
+      );
+
+      user.refreshToken = newRefreshToken;
+      await db.user.update({
+        where: { id: user.id },
+        data: { refreshToken: newRefreshToken },
+      });
+
+      const accessCookieOptions = {
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV !== "development",
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+      };
+
+      const refreshCookieOptions = {
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV !== "development",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      };
+
+      // res.cookie("jwtToken", jwtToken, cookieOptions);
+      res.cookie("accessToken", newAccessToken, accessCookieOptions);
+      res.cookie("refreshToken", newRefreshToken, refreshCookieOptions);
+
+      req.user = accessDecoded;
+      next();
+    }
+  } catch (error) {
+    console.error("Error in isAuthenticated middleware:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
