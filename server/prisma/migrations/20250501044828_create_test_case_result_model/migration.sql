0f3e376f7c1a28682df49c252f678c349685780a
Shreyansh <shreyanshloop07@gmail.com>
2025-05-01 10:19:14 +0530

    feat(models): define TestCaseResult model with relation to Submission

---
+++ server/prisma/migrations/20250501044828_create_test_case_result_model/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "TestCaseResult" (
+    "id" TEXT NOT NULL,
+    "submissionId" TEXT NOT NULL,
+    "testCase" INTEGER NOT NULL,
+    "passed" BOOLEAN NOT NULL,
+    "stdout" TEXT,
+    "expected" TEXT NOT NULL,
+    "stderr" TEXT,
+    "compileOutput" TEXT,
+    "status" TEXT NOT NULL,
+    "memory" TEXT,
+    "time" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "TestCaseResult_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "TestCaseResult_submissionId_idx" ON "TestCaseResult"("submissionId");
+
+-- AddForeignKey
+ALTER TABLE "TestCaseResult" ADD CONSTRAINT "TestCaseResult_submissionId_fkey" FOREIGN KEY ("submissionId") REFERENCES "Submission"("id") ON DELETE CASCADE ON UPDATE CASCADE;
