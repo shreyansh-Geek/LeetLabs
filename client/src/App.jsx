8c231a2b7908570f0a9373177e9b51a4336ae9a3
Shreyansh <shreyanshloop07@gmail.com>
2025-05-16 00:46:19 +0530

    chore(ui): add Toaster component for toast notifications in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import AppRoutes from "./routes/routes.jsx";
 import { Suspense } from "react";
+import { Toaster} from "./components/ui/sonner.jsx";
 
 function App() {
   return (
+    <>
     <Suspense fallback={<div>Loading...</div>}>
       <AppRoutes />
     </Suspense>
+    <Toaster /></>
   );
 }
 
