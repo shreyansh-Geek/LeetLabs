09c423c1c54d01223537490cd2eba922c99f1ae5
Shreyansh <shreyanshloop07@gmail.com>
2025-05-25 17:52:00 +0530

    feat(api-blogs): add  blog api main routes under /api/v1/blogs

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,7 @@ import sheetRoutes from "./routes/sheet.routes.js"
 import noteRoutes from "./routes/note.routes.js";
 import aiDiscussionRoutes from './routes/aiDiscussion.routes.js';
 import glossaryRoutes from './routes/glossary.routes.js';
+import blogRoutes from "./routes/blog.routes.js";
 import cors from 'cors'
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser'
@@ -37,6 +38,7 @@ app.use('/api/v1/sheets', sheetRoutes);
 app.use('/api/v1/notes', noteRoutes);
 app.use('/api/v1/ai-discussions', aiDiscussionRoutes);
 app.use('/api/v1/glossary', glossaryRoutes);
+app.use("/api/v1/blogs", blogRoutes);
 
 app.listen(process.env.PORT|| 3000, () => {
     console.log(`Server running on port ${process.env.PORT}`)   
