f9204f671ff4959feeea39c6f35c30e89247cafb
Shreyansh <shreyanshloop07@gmail.com>
2025-05-27 20:08:27 +0530

    feat(submissions-api): expand submission routes with user-specific and analytical endpoints

--- server/src/routes/submission.routes.js
+++ server/src/routes/submission.routes.js
@@ -1,11 +1,13 @@
 import express from "express"
 import {isAuthenticated} from "../middlewares/auth.middleware.js"
-import {getUserSubmissions, getUserSubmissionsForProblem, getTotalSubmissionsForProblem} from "../controllers/submission.controller.js"
+import { getUserSubmissionsForProblem, getTotalSubmissionsForProblem, getAllUserSubmissions, getStreakData, getPerformanceMetrics,} from "../controllers/submission.controller.js"
 
 const submissionRoutes = express.Router();
 
-submissionRoutes.get('/get-user-submissions', isAuthenticated, getUserSubmissions);
 submissionRoutes.get('/get-user-submissions-for-problem/:problemId', isAuthenticated, getUserSubmissionsForProblem);
 submissionRoutes.get('/get-total-submissions-for-problem/:problemId', isAuthenticated, getTotalSubmissionsForProblem);
+submissionRoutes.get('/get-all-user-submissions', isAuthenticated, getAllUserSubmissions);
+submissionRoutes.get('/get-streak-data', isAuthenticated, getStreakData);
+submissionRoutes.get('/get-performance-metrics', isAuthenticated, getPerformanceMetrics);
 
 export default submissionRoutes
