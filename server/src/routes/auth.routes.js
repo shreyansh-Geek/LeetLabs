45f60380fd31eac8f7a8842e1a89d5dad1df47fe
Shreyansh <shreyanshloop07@gmail.com>
2025-05-29 22:12:14 +0530

    feat(auth): add routes for Google and GitHub OAuth authentication

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { register, verifyUser, login, logout, check, forgotPassword, resetPassword } from '../controllers/auth.controller.js';
+import { register, verifyUser, login, logout, check, forgotPassword, resetPassword, googleAuth, googleCallback, githubAuth, githubCallback } from '../controllers/auth.controller.js';
 import { isAuthenticated } from '../middlewares/auth.middleware.js';
+import passport from 'passport';
 const authRoutes = express.Router();
 
 authRoutes.post('/register', register);
@@ -10,4 +11,9 @@ authRoutes.post('/logout', isAuthenticated, logout);
 authRoutes.get('/check',isAuthenticated, check);
 authRoutes.post('/forgotPassword', forgotPassword);
 authRoutes.post('/resetPassword/:resetPasswordToken', resetPassword);
+authRoutes.get("/google", googleAuth);
+authRoutes.get("/google/callback", passport.authenticate("google", { failureRedirect: `${process.env.FRONTEND_URL}/login` }), googleCallback);
+authRoutes.get("/github", githubAuth);
+authRoutes.get("/github/callback", passport.authenticate("github", { failureRedirect: "/login" }), githubCallback);
+
 export default authRoutes;
